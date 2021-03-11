63664991642e292e5d112e8cdbf3a518087ce27e
Ofek Kazes <ofekkazes2@gmail.com>
2021-03-11 10:38:06 +0100

    Initial Commit

---
+++ npm/work/.license-catalog.mk
@@ -0,0 +1,5 @@
+_LICENSE=APACHE20
+_LICENSE_NAME=Apache License 2.0
+_LICENSE_PERMS=dist-mirror dist-sell pkg-mirror pkg-sell auto-accept
+_LICENSE_GROUPS=FSF OSI
+_LICENSE_DISTFILES=npm-6.14.8.tar.xz
